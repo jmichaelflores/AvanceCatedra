7b67fcfce3bf575db5459cefd33d8f06dd928092
jmichaelflores <jm.michaelflores@gmail.com>
2017-09-15 12:40:05 -0600

    initial commit

---
+++ script/script.js
@@ -0,0 +1,246 @@
+
+
+window.onload = function ()
+
+{
+
+	//AHUACHAPAN
+
+	document.getElementById('ahuachapan').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[0];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('ahuachapan').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+
+		//SONSONATE
+
+		document.getElementById('sonsonate').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[1];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('sonsonate').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//LA LIBERTAD
+
+		document.getElementById('liberta').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[2];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('liberta').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//SAN SALVADOR
+
+		document.getElementById('SS').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[3];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('SS').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//SANTA ANA
+
+		document.getElementById('ana').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[4];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('ana').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}
+
+
+	//CHALATENANGO
+
+		document.getElementById('chalate').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[5];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('chalate').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//CUSCATLAN
+
+		document.getElementById('cusca').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[6];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('cusca').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//LA PAZ
+
+		document.getElementById('paz').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[7];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('paz').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//CABANA
+
+		document.getElementById('caba').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[8];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('caba').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//SAN VICENTE
+
+		document.getElementById('vicente').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[9];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('vicente').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//USULUTAN
+
+		document.getElementById('usu').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[10];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('usu').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//SAN MIGUEL
+
+		document.getElementById('miguel').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[11];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('miguel').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//MORAZAN
+
+		document.getElementById('mora').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[12];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('mora').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+
+
+	//LA UNION
+
+		document.getElementById('union').onmouseover=function(event)
+	{
+		var o=document.getElementsByTagName('span')[13];
+		o.style.left= 15+ event.clientX +"px" ;
+		o.style.top=  15 + event.clientY  +"px" ;
+		o.style.display=  "block" ;
+
+		document.getElementById('union').onmouseout = function()
+		{
+			o.style.display = "none";
+		}
+
+	}	
+}
+
+
